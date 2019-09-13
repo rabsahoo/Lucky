9eeb04d7b6e88334329b2622a8dfeb1c074a17b7
rabsahoo <54200891+rabsahoo@users.noreply.github.com>
2019-09-13 23:41:50 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new Random().Next(1, 10);
+
+           
+            for (var i = 0; i < 4; i++)
+            {
+                Console.Write("Guess the secret number:(between 1 to 10) ");
+                var guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    Console.WriteLine("You won!");
+                    return;
+                }
+            }
+
+            Console.WriteLine("You lost!");
+            Console.WriteLine("Secret is " + number);
+        }
+    }
+}
